3bb1882f4818dfa5e255dcf77ddc66c7e019d0d1
MURA199918 <muralisaranr18@gmail.com>
2020-11-26 13:55:20 +0530

    added UC1

--- src/main/java/com/mechanitis/demo/intellij/MoodAnalyser.java
+++ src/main/java/com/mechanitis/demo/intellij/MoodAnalyser.java
@@ -21,4 +21,11 @@ public class MoodAnalyser {
 
         }
     }*/
+    public String analyseMood(String message){
+        if (message.contains(("sad"))) {
+            return "SAD";
+        } else {
+            return "HAPPY";
+        }
+    }
 }
--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -7,19 +7,19 @@ import org.junit.rules.ExpectedException;
 
 public class MoodAnalyserTest {
 
-   /* @Test
+    @Test
     public void testMoodAnalysis() throws Exception{
-        MoodAnalyser moodAnalyser = new MoodAnalyser("SAD");
-        String mood=moodAnalyser.analyseMood();
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood=moodAnalyser.analyseMood("Sad message");
         Assert.assertThat(mood, CoreMatchers.is("SAD"));
     }
     @Test
     public void testMoodAnalysis1() throws Exception{
-        MoodAnalyser moodAnalyser = new MoodAnalyser("HAPPY");
-        String mood = moodAnalyser.analyseMood();
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("Happy message");
         Assert.assertThat(mood, CoreMatchers.is("HAPPY"));
     }
-    @Test
+    /*@Test
     public void givenNullMoodreturnHappy() throws MoodAnalyserException {
         MoodAnalyser moodAnalyser=new MoodAnalyser(null);
         String mood=moodAnalyser.analyseMood();
