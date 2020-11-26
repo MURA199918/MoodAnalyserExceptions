3e413356a02877cb6847f162fd27615d272433bc
MURA199918 <muralisaranr18@gmail.com>
2020-11-26 13:52:10 +0530

    Mood Analyser testing

---
+++ src/main/java/com/mechanitis/demo/intellij/MoodAnalyser.java
@@ -0,0 +1,24 @@
+package com.mechanitis.demo.intellij;
+
+public class MoodAnalyser {
+    /*String message;
+    public MoodAnalyser(String message){
+        this.message=message;
+    }
+    public String analyseMood(String message) throws MoodAnalyserException {
+        this.message=message;
+        return analyseMood();
+    }
+    public String analyseMood() throws MoodAnalyserException {
+        try {
+            if (message.contains(("sad"))) {
+                return "SAD";
+            } else {
+                return "HAPPY";
+            }
+        } catch (NullPointerException e){
+            throw new MoodAnalyserException("Please enter valid message");
+
+        }
+    }*/
+}
---
+++ src/test/java/MoodAnalyserTest.java
@@ -0,0 +1,42 @@
+import com.mechanitis.demo.intellij.MoodAnalyser;
+import com.mechanitis.demo.intellij.MoodAnalyserException;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class MoodAnalyserTest {
+
+   /* @Test
+    public void testMoodAnalysis() throws Exception{
+        MoodAnalyser moodAnalyser = new MoodAnalyser("SAD");
+        String mood=moodAnalyser.analyseMood();
+        Assert.assertThat(mood, CoreMatchers.is("SAD"));
+    }
+    @Test
+    public void testMoodAnalysis1() throws Exception{
+        MoodAnalyser moodAnalyser = new MoodAnalyser("HAPPY");
+        String mood = moodAnalyser.analyseMood();
+        Assert.assertThat(mood, CoreMatchers.is("HAPPY"));
+    }
+    @Test
+    public void givenNullMoodreturnHappy() throws MoodAnalyserException {
+        MoodAnalyser moodAnalyser=new MoodAnalyser(null);
+        String mood=moodAnalyser.analyseMood();
+        Assert.assertEquals("HAPPY",mood);
+    }
+    @Test
+    public void givennullmoodreturnhappy1(){
+        MoodAnalyser moodAnalyser =new MoodAnalyser(null);
+        String mood=null;
+        try{
+            ExpectedException exceptionRule=ExpectedException.none();
+            exceptionRule.expect(MoodAnalyserException.class);
+            mood=moodAnalyser.analyseMood();
+            Assert.assertEquals("Happy",mood);
+        }catch(MoodAnalyserException e){
+            e.printStackTrace();
+        }
+       // Assert.assertEquals("Happy",mood);
+    }*/
+}
